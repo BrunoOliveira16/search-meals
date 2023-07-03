5d591ab76ae08a75c8ebe63f6cf09b3527208714
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-07-03 11:07:42 -0300

    :sparkles: Feat: Criado Home e MealList, instalado axios e consumido endpoint mealsList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8 items-center justify-center">
+    <input 
+      type="text" 
+      class="rounded border-2 border-gray-200 w-full" 
+      placeholder="Busque por receitas"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter in letters" key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import store from '../store';
+import axiosClient from '../axiosClient.js';
 
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 
-const meals = computed(() => store.state.meals)
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+})
 
 </script>
